9a07a36e931fd6851a8ea37707da0600c80bcbbf
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-09-21 10:32:38 +0100

    frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+	return (
+		<div>
+			<Header />
+			<main>
+				<h3>Hello</h3>
+			</main>
+			<Footer />
+		</div>
+	);
+}
+
+export default App;
