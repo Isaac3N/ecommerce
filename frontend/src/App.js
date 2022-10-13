2aef87697f39a9f09df115d5dba27ef14a84f9ff
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-10-13 14:21:09 +0100

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
 	return (
 		<div>
 			<Header />
-			<main>
-				<h3>Hello</h3>
+			<main className="py-3">
+				<Container>
+					<Route path="/home" component={HomeScreen} exact />
+					<HomeScreen />
+				</Container>
 			</main>
 			<Footer />
 		</div>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,26 @@
 import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
 
 const Header = () => {
 	return (
-		<div>
-			<h1>Header</h1>
-		</div>
+		<header>
+			<Navbar bg="dark" variant="dark" expand="lg" collapseonSelect>
+				<Container>
+					<Navbar.Brand href="/">ProShop</Navbar.Brand>
+					<Navbar.Toggle aria-controls="navbarScroll" />
+					<Navbar.Collapse id="navbarScroll">
+						<Nav className="mr-auto" style={{ maxHeight: "100px" }}>
+							<Nav.Link href="/cart">
+								<i className="fa-solid fa-cart-shopping "></i> Cart
+							</Nav.Link>
+							<Nav.Link href="/login">
+								<i className="fas fa-user "></i> Login
+							</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</header>
 	);
 };
 
