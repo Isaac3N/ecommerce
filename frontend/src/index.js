5afbfbeeac45d171534c2dfc9b787bd3b1424de4
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-10-17 04:54:38 +0100

    updates

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./bootstrap.min.css";
 import "./index.css";
 import App from "./App";
+import store from "./store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<>
+	<Provider store={store}>
 		<App />
-	</>
+	</Provider>
 );
